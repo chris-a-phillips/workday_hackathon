2b6ceceff11889cd7e5bd867b61d680e0b8f19dd
MitchellGRead <mitchell.g.read@gmail.com>
2021-10-01 13:40:54 -0700

    update form to be dynamic

--- src/frontend/Form/Form.js
+++ src/frontend/Form/Form.js
@@ -6,21 +6,30 @@ const Form = () => {
 	const [submitted, setSubmitted] = useState(false);
 	const blankResponse = {
 		user: '',
-		question_one: '',
-		question_two: '',
-		question_three: '',
-		question_four: '',
-		question_five: '',
 	};
 	const [formSection, setFormSection] = useState(0);
 	const [responses, setResponses] = useState(blankResponse);
 
+	const answerGiven = (event) => {
+		handleChange(event)
+		nextSection(event)
+	}
+
 	const handleChange = (event) => {
+		const questionKey = `question_${formSection}`
+		const currResponses = responses
+		currResponses[questionKey] = event.target.value
+
+		setResponses(currResponses);
+	};
+
+	const handleNameChange = (event) => {
 		setResponses({
 			...responses,
-			[event.target.name]: event.target.value,
-		});
-	};
+			'user': event.target.value
+		})
+	}
+
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		setSubmitted(true);
@@ -32,144 +41,52 @@ const Form = () => {
 		setFormSection(formSection + 1);
 	};
 
-	const prevSection = (event) => {
-		event.preventDefault();
-		setFormSection(formSection - 1);
-	};
-
-	const zombie = questions[0];
+	const zombie = questions[1];
 
 	return (
 		<div>
 			<form className="form" onSubmit={handleSubmit}>
 				{formSection === 0 ? (
-					<div>
+					<div className="name-input">
 						<input
 							type='text'
 							name='user'
 							placeholder='Enter Your Name'
-							onChange={handleChange}
+							onChange={handleNameChange}
 							className="input-field"
 						/>
-					</div>
-				) : null}
-				{formSection === 1 ? (
-					<div>
-						<div>
-							<h3>{zombie.questions[0].one}</h3>
-							<label htmlFor='question_one'>yes</label>
-							<input
-								type='radio'
-								name='question_one'
-								value={'yes'}
-								onChange={handleChange}
-							/>
-							<label htmlFor='question_one'>no</label>
-							<input
-								type='radio'
-								name='question_one'
-								value={'no'}
-								onChange={handleChange}
-							/>
-						</div>
-					</div>
-				) : null}
-				{formSection === 2 ? (
-					<div>
-						<div>
-							<h3>{zombie.questions[1].two}</h3>
-							<label htmlFor='question_two'>yes</label>
-							<input
-								type='radio'
-								name='question_two'
-								value={'yes'}
-								onChange={handleChange}
-							/>
-							<label htmlFor='question_two'>no</label>
-							<input
-								type='radio'
-								name='question_two'
-								value={'no'}
-								onChange={handleChange}
-							/>
-						</div>
-					</div>
-				) : null}
-				{formSection === 3 ? (
-					<div>
-						<div>
-							<h3>{zombie.questions[2].three}</h3>
-							<label htmlFor='question_three'>yes</label>
-							<input
-								type='radio'
-								name='question_three'
-								value={'yes'}
-								onChange={handleChange}
-							/>
-							<label htmlFor='question_three'>no</label>
-							<input
-								type='radio'
-								name='question_three'
-								value={'no'}
-								onChange={handleChange}
-							/>
-						</div>
-					</div>
-				) : null}
-				{formSection === 4 ? (
-					<div>
-						<div>
-							<h3>{zombie.questions[3].four}</h3>
-							<label htmlFor='question_four'>yes</label>
-							<input
-								type='radio'
-								name='question_four'
-								value={'yes'}
-								onChange={handleChange}
-							/>
-							<label htmlFor='question_four'>no</label>
-							<input
-								type='radio'
-								name='question_four'
-								value={'no'}
-								onChange={handleChange}
-							/>
-						</div>
-					</div>
-				) : null}
-				{formSection === 5 ? (
-					<div>
-						<div>
-							<h3>{zombie.questions[4].five}</h3>
-							<label htmlFor='question_five'>yes</label>
-							<input
-								type='radio'
-								name='question_five'
-								value={'yes'}
-								onChange={handleChange}
-							/>
-							<label htmlFor='question_five'>no</label>
-							<input
-								type='radio'
-								name='question_five'
-								value={'no'}
-								onChange={handleChange}
-							/>
-						</div>
-					</div>
-				) : null}
-				{formSection <= 4 ? (
-					<div className="question-btns">
-						{formSection > 0 ? (
-							<button className="btn" onClick={prevSection}>Previous</button>
-						) : null}
 						<button className="btn" onClick={nextSection}>Next</button>
 					</div>
-				) : (
-					<div>
-						<button className="btn green-btn" type='submit'>Submit</button>
+				) : null}
+
+				{/* Questions to ask */}
+				{formSection <= zombie.questions.length && formSection > 0 ? (
+					<div className="question">
+						<h3>{zombie.questions[formSection - 1].question}</h3>
+						<div className="question-btns">
+							<button
+								className="btn"
+								value={zombie.questions[formSection - 1].options[0]}
+								onClick={answerGiven}>
+								{zombie.questions[formSection - 1].options[0]}
+							</button>
+
+							<button
+								className="btn"
+								value={zombie.questions[formSection - 1].options[1]}
+								onClick={answerGiven}>
+								{zombie.questions[formSection - 1].options[1]}
+							</button>
+						</div>
 					</div>
-				)}
+				) : null}
+
+				{/* Questions are done */}
+				{formSection > zombie.questions.length ? (
+					<div>
+						<button className="btn green-btn" type='submit'>Submit Responses</button>
+					</div>
+				) : null}
 			</form>
 		</div>
 	);
