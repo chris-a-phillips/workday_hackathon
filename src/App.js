242c52332a4a4a157754fb96559937c25c75b340
Chris Phillips <phillipsachris@gmail.com>
2021-10-01 15:52:30 -0700

    Added chatroom to App

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React from 'react';
 import Landing from './frontend/Landing/Landing';
 import './App.css';
 import Form from './frontend/Form/Form';
+import Room from './frontend/client/Room';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="landing">
-        <Landing />
-      </div>
-      <div className="user-entry-form">
-        <Form />
-      </div>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div className='landing'>
+				<Landing />
+			</div>
+			<div className='user-entry-form'>
+				<Form />
+			</div>
+			<div>
+				<Room />
+			</div>
+		</div>
+	);
 }
 
 export default App;
