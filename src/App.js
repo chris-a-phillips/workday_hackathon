1983fdf1024fe92fd5f220a6f0343a493aabbbbf
MitchellGRead <mitchell.g.read@gmail.com>
2021-10-01 11:15:49 -0700

    update css for App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import Landing from './frontend/Landing/Landing';
-import logo from './logo.svg';
 import './App.css';
 import Form from './frontend/Form/Form';
 
 function App() {
   return (
     <div className="App">
-      <Landing />
-      <Form />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editing <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="landing">
+        <Landing />
+      </div>
+      <div className="user-entry-form">
+        <Form />
+      </div>
     </div>
   );
 }
